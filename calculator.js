54a9632f8e95f355b06e0fcd41ab0d76b4c53c1a
Facundo Fernandez <facundofernanddez@gmail.com>
2023-04-24 19:46:39 -0300

    simple operations add and substract

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const x = require("./operations");
+
+const result1 = x(4, 6);
+
+console.log(result1);
+
+// const result2 = substract(30, 10);
--- test.js
+++
@@ -1,4 +0,0 @@
-const nombre = "Facundo";
-const apellido = "Fernandez";
-
-console.log(`Hola mi nombre es ${nombre} y mi apellido es ${apellido}`);
