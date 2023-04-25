a57e0bf674e06364b2b0f263bcc2c359a428cde7
Facundo Fernandez <facundofernanddez@gmail.com>
2023-04-24 21:13:55 -0300

    learning ES modules

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
-const x = require("./operations");
+import { add, substract } from "./operations.js";
 
-const result1 = x(4, 6);
+const result1 = add(4, 6);
 
 console.log(result1);
 
-// const result2 = substract(30, 10);
+const result2 = substract(30, 10);
+
+console.log(result2);
--- operations.js
+++ operations.js
@@ -1,5 +1,3 @@
-const add = (n1, n2) => n1 + n2;
+export const add = (n1, n2) => n1 + n2;
 
-const substract = (n1, n2) => n1 - n2;
-
-module.exports = add;
+export const substract = (n1, n2) => n1 - n2;
