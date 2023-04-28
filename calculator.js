66f7d1bd4a19b39d828f0dd0c007d495a92e1608
Facundo Fernandez <facundofernanddez@gmail.com>
2023-04-28 20:14:01 -0300

    learning how to install libraries

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 import { add, substract } from "./operations.js";
+import colors from "colors";
 
 const result1 = add(4, 6);
 
-console.log(result1);
+console.log(colors.green(result1));
 
 const result2 = substract(30, 10);
 
