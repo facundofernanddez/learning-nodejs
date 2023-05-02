e47e99bcbce195031f9276799e972a4aabd4a3ac
Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-02 08:32:56 -0300

    devDependencies and nodemon

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
 import { add, substract } from "./operations.js";
 import colors from "colors";
 
-const result1 = add(4, 6);
+const result1 = add(4, 32);
 
-console.log(colors.green(result1));
+console.log(colors.bgBlue(result1));
 
 const result2 = substract(30, 10);
 
